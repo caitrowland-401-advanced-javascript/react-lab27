1ee28404245d9f0cdfe0cca24d9e54ee2e95c6dc
caitleigh <caitleigh27@gmail.com>
2020-03-17 18:55:14 -0700

    updated some CSS so that +/- buttons have a hover effect and counter text is large. Header and footer are now centered on page

---
+++ build/precache-manifest.8cfe79d70d60045c3c4c5f42a60679d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b42ce0250e23cd66b117b7ee7641b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575132215f1aec082885",
+    "url": "/static/css/main.aaf8788a.chunk.css"
+  },
+  {
+    "revision": "b626d2dc5a8b4d788e70",
+    "url": "/static/js/2.4787c189.chunk.js"
+  },
+  {
+    "revision": "575132215f1aec082885",
+    "url": "/static/js/main.4b9c3fbd.chunk.js"
+  },
+  {
+    "revision": "0748ccdbaa5fd979926b",
+    "url": "/static/js/runtime~main.261b9885.js"
+  }
+]);
